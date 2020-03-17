e2eb32616f4c2e174a5bf615b0dc0cccdc6807ea
Krystian Kowalski <Krystian.Kowalski@trw.com>
2020-03-17 20:19:34 +0100

    Fix notifications

--- src/app/navigation/services/notification-employees.service.ts
+++ src/app/navigation/services/notification-employees.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Notification} from '../models/notifiaction';
+import * as data from '../../../assets/notifications.json';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,34 @@ export class NotificationEmployeesService {
   constructor(private restService: HttpClient) { }
 
   getAll(): Observable<Notification[]> {
-    return this.restService.get<Notification[]>('assets/notifications.json');
+    return new Observable<Notification[]>(subscriber => {
+      subscriber.next([
+        {
+          id: '5e70cf2e87574c0c57e3b335',
+          description: 'Kończy się okres umowy',
+          name: 'Jan',
+          surname: 'Kowalski',
+          city: 'Radom',
+          wasClicked: false
+        },
+        {
+          id: '5e70cf2e87574c0c57e3b335',
+          description: 'Kończy się okres umowy',
+          name: 'Jan',
+          surname: 'Kowalski',
+          city: 'Radom',
+          wasClicked: true
+        },
+        {
+          id: '5e70cf2e87574c0c57e3b335',
+          description: 'Kończy się okres umowy',
+          name: 'Jan',
+          surname: 'Kowalski',
+          city: 'Radom',
+          wasClicked: true
+        }
+      ]);
+      subscriber.complete();
+    })
   }
 }
